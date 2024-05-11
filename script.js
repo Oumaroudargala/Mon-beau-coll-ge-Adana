99a91f371877a50e8a8b0e09905c4a8bb2bda9b6
Oumaroudargala <omrdar21@gmail.com>
2024-05-11 11:35:45 +0100

    Add file via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("#toggle.btn");
+const ul = document.getElementById ("asda");
+
+btn.onclick = ()=> ul.style.opacity ==0 ? ul.style.opacity = 1 : ul.style.opacity =0 ;
+
+
+
+
+
+
+
+
+
+ 
